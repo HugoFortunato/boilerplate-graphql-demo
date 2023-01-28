a06a6a8eec715d766ca7477852e7f85e8f784009
Hugo Fortunato <hugo.fortunato@pixter.com.br>
2023-01-28 13:35:40 -0300

    feat: config styled-components, bring the configs from styled components for nextjs and jest

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,14 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="logo" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Ilustration src="/img/hero-illustration.svg" alt="desenvolvedor" />
+  </S.Wrapper>
 )
 
 export default Main
