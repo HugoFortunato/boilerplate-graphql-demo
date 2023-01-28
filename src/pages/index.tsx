89358fb1df5e9703f0db53156a826e017db608db
Hugo Fortunato <hugo.fortunato@pixter.com.br>
2023-01-28 09:12:16 -0300

    feat: Add husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
 type Props = {
   title: string
 }
-
-const bla = 'var'
 
 export default function Home({ title = 'React Avançado' }: Props) {
   return <h1>{title}</h1>
